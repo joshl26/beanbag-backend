b9b0af2066faeab9267de617c35a608c060c7205
Joshua Lehman <joshlehman.dev@gmail.com>
2023-12-22 13:17:56 -0500

    updates

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,10 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS ||
+  "http://localhost:7000,http://localhost:7001" ||
+  "148.113.181.200" ||
+  "99.246.75.50";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS =
